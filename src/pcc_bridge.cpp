cbb1e932c41fe043239449ccee8e362f6e0e8f5d
Hunter Baker <62899372+literally-anything@users.noreply.github.com>
2024-10-02 21:41:37 -0400

    Fix placement of timer reset and fix tabs

--- src/pcc_bridge.cpp
+++ src/pcc_bridge.cpp
@@ -29,93 +29,93 @@ namespace pcc_bridge {
         port.close();
 
         while (!port.isOpen()) {
-	        for (directory_entry& entry : directory_iterator("/dev")) {
-	            std::string name = entry.path().filename().string();
+            for (directory_entry& entry : directory_iterator("/dev")) {
+                std::string name = entry.path().filename().string();
 
-	            if (name.rfind("ttyACM", 0) == 0) {
-	                try {
-		                port.setPort(entry.path().c_str());
-		                port.open();
-		                RCLCPP_INFO(get_logger(), "Port Opened");
-		                break;
-		            } catch (...) {}
-	            }
-	        }
+                if (name.rfind("ttyACM", 0) == 0) {
+                    try {
+                        port.setPort(entry.path().c_str());
+                        port.open();
+                        RCLCPP_INFO(get_logger(), "Port Opened");
+                        break;
+                    } catch (...) {}
+                }
+            }
         }
     }
 
     void PCCBridgeNode::readLoop() {
         readTimer->cancel();
         if (!isOpening.load()) {
-	        try {
-		    	if (port.isOpen()) {
-		    	    uint8_t currentByte;
-		            if (port.available() > 0) {
-		                if (dataQueue.size() >= MESSAGE_BUF_LEN) {
-		                    dataQueue.erase(dataQueue.cbegin());
-		                }
-		                
-		                port.read(&currentByte, 1);
-		                dataQueue.push_back(currentByte);
-		            }
+            try {
+                if (port.isOpen()) {
+                    uint8_t currentByte;
+                    if (port.available() > 0) {
+                        if (dataQueue.size() >= MESSAGE_BUF_LEN) {
+                            dataQueue.erase(dataQueue.cbegin());
+                        }
+                        
+                        port.read(&currentByte, 1);
+                        dataQueue.push_back(currentByte);
+                    }
 
-		    	    if (dataQueue.size() == MESSAGE_BUF_LEN && dataQueue[0] == 255 && dataQueue[1] == 255) {
-		    	        const auto message = reinterpret_cast<message_t *>(dataQueue.data());
+                    if (dataQueue.size() == MESSAGE_BUF_LEN && dataQueue[0] == 255 && dataQueue[1] == 255) {
+                        const auto message = reinterpret_cast<message_t *>(dataQueue.data());
 
-		    	        if (checkCrc(message)) {
-		    	            dataQueue.clear();
-		    	            switch (message->identifier) {
-		    	              case TOPIC_STATUS:
-		                          switch (message->data[0]) {
-		                              case STATUS_READY:
-		                                  ledComponent.sendUpdate();
-		                                  servoComponent.sendUpdate();
-		                                  RCLCPP_INFO(get_logger(), "PCC Ready");
-		                                  break;
-		                              case STATUS_RESET:
-		                                  RCLCPP_INFO(get_logger(), "PCC Resetting");
-		                                  break;
-		                              case STATUS_HALT:
-		                                  RCLCPP_ERROR(get_logger(), "PCC Halted");
-		                          }
-		    	                  break;
-		                      case TOPIC_ERROR:
-		                          RCLCPP_ERROR(get_logger(), "Got error: %u", message->data[0]);
-		                          switch (message->data[0]) {
-		                          	case COMPONENT_THERMAL:
-		                          	    if (message->data[1] == ERROR_TYPE_RESOLVED) {
-		                          	        RCLCPP_INFO(get_logger(), "Thermal camera has connected");
-		                          	    } else {
-		                          	    	RCLCPP_ERROR(get_logger(), "Thermal camera has disconnected");
-		                          	    }
-		                          	    break;
-		                          	case COMPONENT_SERVOS:
-		                          	    if (message->data[1] == ERROR_TYPE_RESOLVED) {
-		                          	        RCLCPP_INFO(get_logger(), "Servo controller has connected");
-		                          	    } else {
-		                          	    	RCLCPP_ERROR(get_logger(), "Servo controller has disconnected");
-		                          	    }
-		                          }
-		                          break;
-		                      default:
-		                          RCLCPP_WARN(get_logger(), "Invalid topic: %ul", message->identifier);
-		                    }
-		    	        }
-		    	    }
-		    	} else {
-		    	    throw std::exception();
-		    	}
-	    	} catch (...) {
-		        RCLCPP_WARN(get_logger(), "Port not open. Searching...");
-		        isOpening = true;
-	    		openPort();
-	    		isOpening = false;
+                        if (checkCrc(message)) {
+                            dataQueue.clear();
+                            switch (message->identifier) {
+                              case TOPIC_STATUS:
+                                  switch (message->data[0]) {
+                                      case STATUS_READY:
+                                          ledComponent.sendUpdate();
+                                          servoComponent.sendUpdate();
+                                          RCLCPP_INFO(get_logger(), "PCC Ready");
+                                          break;
+                                      case STATUS_RESET:
+                                          RCLCPP_INFO(get_logger(), "PCC Resetting");
+                                          break;
+                                      case STATUS_HALT:
+                                          RCLCPP_ERROR(get_logger(), "PCC Halted");
+                                  }
+                                  break;
+                              case TOPIC_ERROR:
+                                  RCLCPP_ERROR(get_logger(), "Got error: %u", message->data[0]);
+                                  switch (message->data[0]) {
+                                      case COMPONENT_THERMAL:
+                                          if (message->data[1] == ERROR_TYPE_RESOLVED) {
+                                              RCLCPP_INFO(get_logger(), "Thermal camera has connected");
+                                          } else {
+                                              RCLCPP_ERROR(get_logger(), "Thermal camera has disconnected");
+                                          }
+                                          break;
+                                      case COMPONENT_SERVOS:
+                                          if (message->data[1] == ERROR_TYPE_RESOLVED) {
+                                              RCLCPP_INFO(get_logger(), "Servo controller has connected");
+                                          } else {
+                                              RCLCPP_ERROR(get_logger(), "Servo controller has disconnected");
+                                          }
+                                  }
+                                  break;
+                              default:
+                                  RCLCPP_WARN(get_logger(), "Invalid topic: %ul", message->identifier);
+                            }
+                        }
+                    }
+                } else {
+                    throw std::exception();
+                }
+            } catch (...) {
+                RCLCPP_WARN(get_logger(), "Port not open. Searching...");
+                isOpening = true;
+                openPort();
+                isOpening = false;
 
-	            ledComponent.sendUpdate();
-	            servoComponent.sendUpdate();
-		    }
-		    readTimer->reset();
-	    }
+                ledComponent.sendUpdate();
+                servoComponent.sendUpdate();
+            }
+        }
+        readTimer->reset();
     }
 
     void PCCBridgeNode::sendMessage(message_t *message) {
@@ -124,11 +124,11 @@ namespace pcc_bridge {
         append_crc(message);
 
         if (!isOpening.load()) {
-	        try {
-	            port.write(reinterpret_cast<uint8_t *>(message), MESSAGE_BUF_LEN);
-	        } catch (...) {
-	            RCLCPP_ERROR(get_logger(), "Failed to send message using serial port");
-	        }
+            try {
+                port.write(reinterpret_cast<uint8_t *>(message), MESSAGE_BUF_LEN);
+            } catch (...) {
+                RCLCPP_ERROR(get_logger(), "Failed to send message using serial port");
+            }
         }
     }
 }
