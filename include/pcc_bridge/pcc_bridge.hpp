f29d575ae6f9188e51defccb1f646be6f98312d3
Hunter Baker <62899372+literally-anything@users.noreply.github.com>
2024-10-02 22:17:18 -0400

    Add reset service

--- include/pcc_bridge/pcc_bridge.hpp
+++ include/pcc_bridge/pcc_bridge.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <rclcpp/rclcpp.hpp>
 #include <serial/serial.h>
+#include <std_msgs/msg/empty.hpp>
+#include <std_srvs/srv/trigger.hpp>
 
 #include "pcc_bridge/comm.hpp"
 
@@ -30,6 +32,11 @@ namespace pcc_bridge
 
         LedComponent ledComponent;
         ServoComponent servoComponent;
+
+        rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr resetService;
+
+        void resetCallback(const std::shared_ptr<std_srvs::srv::Trigger::Request> _,
+                           std::shared_ptr<std_srvs::srv::Trigger::Response> _);
 
         void openPort();
         void sendMessage(message_t *message);
